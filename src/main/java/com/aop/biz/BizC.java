8f1e9deb85ba74105f987720cd9c3cd0da92e0b5
wanghe9011 <wanghe9011@sina.com>
2020-07-14 17:17:22 +0800

    init

---
+++ src/main/java/com/aop/biz/BizC.java
@@ -0,0 +1,16 @@
+package com.aop.biz;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: WangHe
+ * @Date: 2020/7/13 16:29
+ */
+@Service
+public class BizC {
+
+    public void doC() {
+
+    }
+
+}
